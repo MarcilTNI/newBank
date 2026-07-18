21d2684cc205dcad27b894a9b8b414af0edaa03e
Marcel Ibrahim <marcel29051998@gmail.com>
2026-07-18 19:31:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const beranda = document.querySelector('.main-content-beranda')
+const riwayat = document.querySelector('.main-content-riwayat')
+
+const btnShowBeranda = document.querySelector('#btnBeranda')
+const btnShowRiwayat = document.querySelector('#btnRiwayat')
+
+// btnShowBeranda.addEventListener('click', () => {
+//   riwayat.style.display = 'none'
+//   beranda.style.display = 'flex'
+// })
+
+// btnShowRiwayat.addEventListener('click', () => {
+//   beranda.style.display = 'none'
+//   riwayat.style.display = 'flex'
+// })

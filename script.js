acdb81bac3b3c12caeb66b46a9c890b428d62724
Marcel Ibrahim <marcel29051998@gmail.com>
2026-07-21 20:51:27 +0700

    Enhance favorites section UI with style adjustments and improved layout

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ const btnTambahFavorit = document.querySelector("#btnTambahFavorit");
 const btnLogin = document.querySelector("#btnLogin");
 const btnDaftar = document.querySelector("#btnDaftar");
 
-const pageBeranda = document.querySelector('.main-content-beranda')
-const pageFavorit = document.querySelector('.main-content-favorit')
-const pageRiwayat = document.querySelector('.main-content-riwayat')
-const pageProfil = document.querySelector('.main-content-profil')
+const pageBeranda = document.querySelector(".main-content-beranda");
+const pageFavorit = document.querySelector(".main-content-favorit");
+const pageRiwayat = document.querySelector(".main-content-riwayat");
+const pageProfil = document.querySelector(".main-content-profil");
 
 const btnPindahLogin = document.querySelector("#pindahLogin");
 const btnPindahDaftar = document.querySelector("#pindahDaftar");
@@ -39,8 +39,9 @@ const btnFavorit = document.querySelector("#btnFavorit");
 const btnRiwayat = document.querySelector("#btnRiwayat");
 const btnProfil = document.querySelector("#btnProfil");
 
-const showTopUp = document.querySelector("#showTopUp");
+const listFavorit = document.querySelector("#listFavorit");
 
+const showTopUp = document.querySelector("#showTopUp");
 
 let users = [];
 let currentUser = null;
@@ -59,15 +60,15 @@ function getUsers() {
 }
 
 function resetAll() {
-  const pages = document.querySelectorAll('.page-content')
-  pages.forEach(page => {
-    page.style.display = 'none'
-  })
+  const pages = document.querySelectorAll(".page-content");
+  pages.forEach((page) => {
+    page.style.display = "none";
+  });
 
-  const buttons = document.querySelectorAll('.btn-navbar-bottom')
-  buttons.forEach(btn => {
-    btn.classList.remove('active')
-  })
+  const buttons = document.querySelectorAll(".btn-navbar-bottom");
+  buttons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 }
 
 function updateUserUI() {
@@ -81,29 +82,24 @@ function daftar() {
   const pass1 = inputPasswordDaftar1.value.trim();
   const pass2 = inputPasswordDaftar2.value.trim();
 
-  // const cekDoubleName = username.toLowerCase()
-
   if (!username || !pass1 || !pass2) {
-    showPopup("Nama dan Password tidak boleh kosong", 'error');
+    showPopup("Nama dan Password tidak boleh kosong", "error");
     return;
   }
 
   if (pass1 !== pass2) {
-    showPopup("Password berbeda", 'error');
+    showPopup("Password berbeda", "error");
     return;
   }
 
-  const userExist = users.find((u) => u.nama === username);
+  const usernameLower = username.toLowerCase();
+
+  const userExist = users.find((u) => u.nama?.toLowerCase() === usernameLower);
 
   if (userExist) {
-    showPopup("Nama sudah terdaftar!", 'error');
+    showPopup("Nama sudah terdaftar!", "error");
     return;
   }
-
-  // if(username.toLowerCase() === cekDoubleName) {
-  //   showPopup('Nama sudh terdaftar!', 'error')
-  //   return
-  // }
 
   const newUser = {
     id: `USR-${Date.now().toString().slice(-6)}-${Math.floor(Math.random() * 1000)}`,
@@ -118,7 +114,7 @@ function daftar() {
 
   saveUsers();
 
-  showPopup("Pendaftaran berhasil, silahkan Login!", 'success');
+  showPopup("Pendaftaran berhasil, silahkan Login!", "success");
 
   inputNamaDaftar.value = "";
   inputPasswordDaftar1.value = "";
@@ -132,14 +128,14 @@ async function login() {
   const pass = inputPasswordLogin.value.trim();
 
   if (!username || !pass) {
-    showPopup("Nama dan Password tidak boleh kosong!", 'error');
+    showPopup("Nama dan Password tidak boleh kosong!", "error");
     return;
   }
 
   const userExsist = users.find((u) => u.nama === username);
 
   if (!userExsist) {
-    showPopup("User tidak ditemukan!", 'error');
+    showPopup("User tidak ditemukan!", "error");
     return;
   }
 
@@ -157,10 +153,10 @@ async function login() {
 
     currentUser = validUser;
     updateUserUI();
-    renderFavorit()
+    renderFavorit();
     console.log(currentUser);
   } catch (error) {
-    showPopup("Nama atau Password salah!", 'error');
+    showPopup("Nama atau Password salah!", "error");
   } finally {
     showLoading(false);
   }
@@ -178,6 +174,9 @@ function closeFormTopUp() {
   const formTopUp = document.querySelector("#topUpForm");
   formTopUp.style.display = "none";
   formTopUpTerbuka = false;
+
+  inputNominal.value = "";
+  inputBank.value = "";
 }
 
 function topUpSaldo() {
@@ -185,16 +184,16 @@ function topUpSaldo() {
   const bank = inputBank.value;
 
   if (isNaN(nominal)) {
-    showPopup("Masukkan angka yang valid!", 'error');
+    showPopup("Masukkan angka yang valid!", "error");
   }
 
   if (!nominal || !bank) {
-    showPopup("Form tidak boleh kosong!", 'error');
+    showPopup("Form tidak boleh kosong!", "error");
     return;
   }
 
   if (nominal < 10000) {
-    showPopup("Minimal topUp Rp10.000", 'error');
+    showPopup("Minimal topUp Rp10.000", "error");
     return;
   }
 
@@ -202,7 +201,7 @@ function topUpSaldo() {
 
   updateUserUI();
 
-  showPopup("Berhasil Topup", 'success');
+  showPopup("Berhasil Topup", "success");
 
   const formTopUp = document.querySelector("#topUpForm");
   formTopUp.style.display = "none";
@@ -215,73 +214,112 @@ function topUpSaldo() {
 }
 
 function tambahFavorit() {
-  const nominal = Number(inputNominal.value);
-  const bank = inputBank.value;
+  const nominal = Number(inputNominal.value.trim());
+  const bankInput = inputBank.value.trim().toLowerCase();
 
   if (isNaN(nominal)) {
-    showPopup("Masukkan angka yang valid!", 'error');
+    showPopup("Masukkan angka yang valid!", "error");
     return;
   }
 
-  if (!nominal || !bank) {
-    showPopup("Form tidak boleh kosong!", 'error');
+  if (!nominal || !bankInput) {
+    showPopup("Form tidak boleh kosong!", "error");
     return;
   }
 
   if (nominal < 10000) {
-    showPopup("Minimal topUp Rp10.000", 'error');
+    showPopup("Minimal topUp Rp10.000", "error");
     return;
+  }
+
+  const favExsist = currentUser.favorit.find((fav) => {
+    const favBankLower = fav.bank.trim().toLowerCase();
+    return fav.nominal === nominal && favBankLower === bankInput;
+  });
+
+  if(favExsist) {
+    showPopup('Sudah pernah ditambahkan ke favorit!', 'error')
+    return
   }
 
   const newFavorit = {
     id: `FAV-${Date.now().toString().slice(-6)}-${Math.floor(Math.random() * 1000)}`,
     nominal: nominal,
-    bank: bank,
+    bank: inputBank.value.trim(),
   };
 
   currentUser.favorit.push(newFavorit);
 
-  saveUsers()
+  saveUsers();
 
-  showPopup("Berhasil di tambah ke Favorit", 'success');
+  showPopup("Berhasil di tambah ke Favorit", "success");
   console.log(currentUser);
-  renderFavorit()
+  renderFavorit();
 }
 
 function renderFavorit() {
-  const listFavorit = document.querySelector('#listFavorit')
+  const listFavorit = document.querySelector("#listFavorit");
 
-  let html = ''
+  let html = "";
 
-  currentUser.favorit.forEach(fav => {
+  currentUser.favorit.forEach((fav) => {
     html += `
-      <li class="item-favorit"> 
+      <li class="item-favorit" data-id="${fav.id}"> 
         <div class="box-text">
           <p>${fav.bank}</p>
-          <p>Rp ${fav.nominal.toLocaleString('id-ID')}</p>
+          <p>Rp ${fav.nominal.toLocaleString("id-ID")}</p>
         </div>
         <button class="pakai-lagi">Pakai lagi</button>
         <button class="hapus-favorit">Hapus</button>
       </li>
-    `
+    `;
   });
 
-  listFavorit.innerHTML = 
-    html || `<li style="list-style-type: none; color: gray; font-size: 13px; font-weight: bold; font-style: italic;">Belum ada favorit</li> `
+  listFavorit.innerHTML =
+    html ||
+    `<li style="list-style-type: none; color: gray; font-size: 13px; font-weight: bold; font-style: italic;">Belum ada favorit</li> `;
 }
 
+function hapusListFavorit(id) {
+  currentUser.favorit = currentUser.favorit.filter((fav) => fav.id !== id);
+  renderFavorit();
+  saveUsers();
+}
+
+listFavorit.addEventListener("click", (e) => {
+  const itemFavorit = e.target.closest(".item-favorit");
+  if (!itemFavorit) return;
+
+  const id = itemFavorit.dataset.id;
+
+  if (e.target.classList.contains("hapus-favorit")) {
+    hapusListFavorit(id);
+  }
+
+  if (e.target.classList.contains("pakai-lagi")) {
+    const selectedFavorit = currentUser.favorit.find((fav) => fav.id === id);
+
+    if (selectedFavorit) {
+      inputNominal.value = selectedFavorit.nominal;
+      inputBank.value = selectedFavorit.bank;
+
+      showFormTopUp();
+    }
+  }
+});
+
 function showPopup(message, type) {
-  popup.textContent = message
+  popup.textContent = message;
 
-  popup.classList.remove('success', 'error')
-  popup.classList.add(type)
-  popup.classList.add('show')
+  popup.classList.remove("success", "error");
+  popup.classList.add(type);
+  popup.classList.add("show");
 
-  clearTimeout(popupTimer)
+  clearTimeout(popupTimer);
 
   popupTimer = setTimeout(() => {
-    popup.classList.remove('show')
-  }, 2000)
+    popup.classList.remove("show");
+  }, 2000);
 }
 
 function showLoading(state) {
@@ -314,31 +352,31 @@ function showLoading(state) {
 }
 
 function showBeranda() {
-  resetAll()
-  pageBeranda.style.display = 'flex'
-  btnBeranda.classList.add('active')
-  console.log('klik')
+  resetAll();
+  pageBeranda.style.display = "flex";
+  btnBeranda.classList.add("active");
+  console.log("klik");
 }
 
 function showFavorit() {
-  resetAll()
-  pageFavorit.style.display = 'flex'
-  btnFavorit.classList.add('active')
-  console.log('klik')
+  resetAll();
+  pageFavorit.style.display = "flex";
+  btnFavorit.classList.add("active");
+  console.log("klik");
 }
 
 function showRiwayat() {
-  resetAll()
-  pageRiwayat.style.display = 'flex'
-  btnRiwayat.classList.add('active')
-  console.log('klik')
+  resetAll();
+  pageRiwayat.style.display = "flex";
+  btnRiwayat.classList.add("active");
+  console.log("klik");
 }
 
 function showProfil() {
-  resetAll()
-  pageProfil.style.display = 'flex'
-  btnProfil.classList.add('active')
-  console.log('klik')
+  resetAll();
+  pageProfil.style.display = "flex";
+  btnProfil.classList.add("active");
+  console.log("klik");
 }
 
 btnLogin.addEventListener("click", login);
@@ -358,10 +396,9 @@ btnTambahFavorit.addEventListener("click", tambahFavorit);
 
 btnTambahSaldo.addEventListener("click", topUpSaldo);
 
-btnBeranda.addEventListener('click', showBeranda)
-btnFavorit.addEventListener('click', showFavorit)
-btnRiwayat.addEventListener('click', showRiwayat)
-btnProfil.addEventListener('click', showProfil)
+btnBeranda.addEventListener("click", showBeranda);
+btnFavorit.addEventListener("click", showFavorit);
+btnRiwayat.addEventListener("click", showRiwayat);
+btnProfil.addEventListener("click", showProfil);
 
-
-getUsers()
+getUsers();
